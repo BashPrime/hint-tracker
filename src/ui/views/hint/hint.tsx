2bd41249c27e6bbdd78afe63c5d45058a2b2160e
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-01-29 00:59:51 -0800

    wire in hint atoms, right click

--- src/ui/views/hint/hint.tsx
+++ src/ui/views/hint/hint.tsx
@@ -1,17 +1,40 @@
 import { AtomCombobox } from '@/components/atom-combobox';
+import { useRightClick } from '@/hooks/useRightClick';
 import { cn } from '@/lib/utils';
 import { Hint as HintType } from '@/types/hint-layout.types';
+import { useAtom } from 'jotai';
 
 type Props = {
   hint: HintType;
 };
 
 export function Hint({ hint }: Props) {
+  // !STATE
+  const [checked, setChecked] = useAtom(hint.checked);
+
+  // !HOOK
+  const handleRightClick = useRightClick(() => setChecked(!checked));
+
   return (
-    <div className="border border-black">
+    <div
+      className={cn('border border-black', checked && 'bg-green-900')}
+      onMouseDown={handleRightClick}
+    >
       <p className={cn(hint.color && `text-${hint.color}`)}>{hint.name}</p>
-      {hint.item && <AtomCombobox atom={hint.item} placeholder={"What?"} items={['Dark Beam']} />}
-      {hint.location && <AtomCombobox atom={hint.location} placeholder={"Where?"} items={['Torvus Energy Controller']} />}
+      {hint.item && (
+        <AtomCombobox
+          atom={hint.item}
+          placeholder={'What?'}
+          items={['Dark Beam']}
+        />
+      )}
+      {hint.location && (
+        <AtomCombobox
+          atom={hint.location}
+          placeholder={'Where?'}
+          items={['Torvus Energy Controller']}
+        />
+      )}
     </div>
   );
 }
