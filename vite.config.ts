2e8f236369df50759b91502efd1330cd543160cb
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-02-06 23:15:55 -0800

    tanstack file routes template

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './src/ui/routes',
+      generatedRouteTree: './src/ui/routeTree.gen.ts'
+    }),
+    react(),
+    tailwindcss(),
+  ],
   base: './',
   build: {
     outDir: 'dist-react',
