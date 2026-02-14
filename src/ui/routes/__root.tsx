24b4393fa7c3cc873934cdba8ec6f8d15ed5f504
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-02-13 23:36:41 -0800

    basic breadcrumbs thingy

--- src/ui/routes/__root.tsx
+++ src/ui/routes/__root.tsx
@@ -1,14 +1,50 @@
 import { useThemeChanger } from '@/hooks/useThemeChanger';
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { activeGameState, activeLayoutState } from '@/states/App.states';
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { useAtomValue } from 'jotai';
+import { ChevronRight, Home } from 'lucide-react';
 import './root.css';
 
 function RootLayout() {
+  // !STATE
+  const game = useAtomValue(activeGameState);
+  const layout = useAtomValue(activeLayoutState);
+
   // !HOOKS
   useThemeChanger();
 
   return (
     <>
+      <nav className="flex flex-none items-center gap-2 font-bold">
+        <Link to="/">
+          <Home />
+        </Link>
+        {game && (
+          <>
+            <ChevronRight />
+            <Link
+              to="/games/$gameId"
+              params={{ gameId: game.id }}
+              className="hover:text-blue-400"
+            >
+              {game.name}
+            </Link>
+          </>
+        )}
+        {layout && (
+          <>
+            <ChevronRight />
+            <Link
+              to="/layouts/$layoutId"
+              params={{ layoutId: layout.id }}
+              className="hover:text-blue-400"
+            >
+              {layout.name}
+            </Link>
+          </>
+        )}
+      </nav>
       <div className="flex-auto overflow-auto">
         <Outlet />
       </div>
