ce2af589909f22b7578c2ad1a84c1fc8bc08d88b
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-01 18:21:41 -0700

    "no packs installed" message

--- src/ui/routes/index.tsx
+++ src/ui/routes/index.tsx
@@ -1,16 +1,17 @@
 import { GameCover } from '@/components/game-cover';
 import { LoadingSpinner } from '@/components/loading-spinner';
-import { fetchCovers, fetchGames, fetchPacks } from '@/ipc';
+import { fetchCovers, fetchGames } from '@/ipc';
 import { cn } from '@/lib/utils';
 import { packsState } from '@/states/App.states';
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { useAtomValue } from 'jotai';
+import { Frown } from 'lucide-react';
 
 export const Route = createFileRoute('/')({
   component: Index,
   pendingComponent: LoadingSpinner,
   loader: async () => {
-    await fetchPacks();
+    // await fetchPacks();
     await fetchGames();
     await fetchCovers();
   },
@@ -20,36 +21,50 @@ function Index() {
   // !STATE
   const packs = useAtomValue(packsState);
 
-  if (!packs) {
-    return null;
-  }
-
   return (
     <div data-name="index-root">
-      <p className="p-2 text-center text-2xl font-bold uppercase">
-        Select Game
-      </p>
-      <div className="flex flex-wrap justify-center gap-4 p-2">
-        {packs?.map((pack) => {
-          return (
-            <Link
-              to="/games/$gameId"
-              params={{ gameId: pack.data.id }}
-              key={pack.data.id}
-              className={cn(
-                'flex flex-col items-center',
-                'relative',
-                'after:absolute after:inset-0 hover:after:bg-blue-400/50'
-              )}
-            >
-              {pack.cover && <GameCover name={pack.cover.name} image={pack.cover} className="h-72" />}
-              <p className="max-w-48 text-center text-xl font-semibold">
-                {pack.data.name}
-              </p>
-            </Link>
-          );
-        })}
-      </div>
+      {!packs && (
+        <div className="flex flex-col items-center">
+          <p className="p-2 text-center text-2xl font-bold uppercase">
+            No Packs Installed!
+          </p>
+          <Frown size={48} />
+        </div>
+      )}
+      {packs && (
+        <>
+          <p className="p-2 text-center text-2xl font-bold uppercase">
+            Select Pack
+          </p>
+          <div className="flex flex-wrap justify-center gap-4 p-2">
+            {packs.map((pack) => {
+              return (
+                <Link
+                  to="/games/$gameId"
+                  params={{ gameId: pack.data.id }}
+                  key={pack.data.id}
+                  className={cn(
+                    'flex flex-col items-center',
+                    'relative',
+                    'after:absolute after:inset-0 hover:after:bg-blue-400/50'
+                  )}
+                >
+                  {pack.cover && (
+                    <GameCover
+                      name={pack.cover.name}
+                      image={pack.cover}
+                      className="h-72"
+                    />
+                  )}
+                  <p className="max-w-48 text-center text-xl font-semibold">
+                    {pack.data.name}
+                  </p>
+                </Link>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
