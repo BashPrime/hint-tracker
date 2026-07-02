ce9f41b719761f80d2b0736b5d65c1c7e49acdf4
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-01 19:51:52 -0700

    clean up atoms

--- src/ui/states/App.states.ts
+++ src/ui/states/App.states.ts
@@ -1,30 +1,5 @@
-import { Layout } from '@/types/layout.types';
-import { GameDataOptionsTransformSchema } from '@/types/transform.types';
 import { atom } from 'jotai';
-import { Cover } from 'src/shared/types/cover.types';
-import { Game } from 'src/shared/types/game.types';
 import { BasicPackData } from 'src/shared/types/pack.type';
-import { Preset } from 'src/shared/types/preset.types';
 
 export const packsState = atom<BasicPackData[] | null>(null);
 export const activePackState = atom<BasicPackData | null>(null);
-
-export const presetsState = atom<Preset[] | null>(null);
-export const activePresetState = atom<Preset | null>(null);
-
-export const gamesState = atom<Game[] | null>(null);
-export const activeGameState = atom<Game | null>(null);
-
-export const activeGameDataOptionsSelector = atom((get) => {
-  const game = get(activeGameState);
-
-  if (game) {
-    return GameDataOptionsTransformSchema.parse(game?.data);
-  }
-
-  return null;
-});
-
-export const coversState = atom<Cover[] | null>(null);
-
-export const activeLayoutState = atom<Layout | null>(null);
