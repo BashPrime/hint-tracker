387fe3946443ce2b90aa654f5e5bca80c1ad1c44
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-03 02:13:54 -0700

    autocomplete checkpoint

--- src/ui/views/layout/hint.tsx
+++ src/ui/views/layout/hint.tsx
@@ -1,3 +1,6 @@
+import { AtomCombobox } from '@/components/atom-combobox';
+import { useAutofills } from '@/hooks/useAutofills';
+import { atom } from 'jotai';
 import {
   Autofills,
   LayoutHint as LayoutHintType,
@@ -8,12 +11,28 @@ type Props = {
   autofills?: Autofills;
 };
 
+const itemAtom = atom('');
+const locationAtom = atom('');
+
 export function LayoutHint({ hint, autofills }: Props) {
+  // !STATE
+  
+  // !HOOKS
+  const { location } = useAutofills();
+
   return (
     <div data-name="layout-hint">
       <p className="font-semibold">{hint.name}</p>
-      {hint.hintType !== 'location' && <p>Item</p>}
-      {hint.hintType !== 'item' && <p>Location</p>}
+      {hint.hintType !== 'location' && (
+        <AtomCombobox atom={itemAtom} placeholder={'Item'} items={[]} />
+      )}
+      {hint.hintType !== 'item' && (
+        <AtomCombobox
+          atom={locationAtom}
+          placeholder={'Location'}
+          items={location.map((l) => l.name)}
+        />
+      )}
     </div>
   );
 }
