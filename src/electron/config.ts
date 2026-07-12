b0866d26ad96ae829d46a7516d8c9346e5f89a21
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-12 00:02:54 -0500

    make sure dir exists when saving tracker state

--- src/electron/config.ts
+++ src/electron/config.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { dirname } from 'path';
 import { ConfigSchema, ConfigType } from '../shared/types/config.types.js';
 import {
   CONFIG_PATH,
@@ -36,14 +37,23 @@ export function handleCreateUserDataDirs() {
 }
 
 export function saveTrackerState(state: object, path: string) {
-  const json = JSON.stringify(state, null, 2);
-  fs.writeFile(path, json, (err) => {
+  // Make sure directory exists first
+  const dir = dirname(path);
+  fs.mkdir(dir, { recursive: true }, (err) => {
     if (err) {
-      console.error(
-        'saveTrackerState(): Error writing json file:',
-        path,
-        getErrorMsg(err)
-      );
+      return console.error(err);
+    } else {
+      // Handle writing file
+      const json = JSON.stringify(state, null, 2);
+      fs.writeFile(path, json, (err) => {
+        if (err) {
+          console.error(
+            'saveTrackerState(): Error writing json file:',
+            path,
+            getErrorMsg(err)
+          );
+        }
+      });
     }
   });
 }
