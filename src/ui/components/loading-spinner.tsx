f9a881eb62ee987dda8ae95054d4a178b296c8cc
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-12 22:50:04 -0700

    support custom text for loading spinner

--- src/ui/components/loading-spinner.tsx
+++ src/ui/components/loading-spinner.tsx
@@ -1,12 +1,18 @@
 import { cn } from '@/lib/utils';
 import { Spinner } from './ui/spinner';
 
-export function LoadingSpinner() {
+type Props = {
+  text?: string
+}
+
+export function LoadingSpinner({ text }: Props) {
   return (
     <div className={cn('flex justify-center')} data-name="loading-spinner">
       <div className="flex flex-col items-center">
         <Spinner className="size-32" />
-        <p className={cn('text-4xl')}>Loading...</p>
+        <p className={cn('text-4xl')}>
+          {text ? text : 'Loading...'}
+          </p>
       </div>
     </div>
   );
