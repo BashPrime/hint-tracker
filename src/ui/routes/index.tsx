253f5eaaaaa42b5885133d9832f9ca2c5d7cc36d
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-15 21:03:47 -0700

    oops forgot to check this in

--- src/ui/routes/index.tsx
+++ src/ui/routes/index.tsx
@@ -1,11 +1,9 @@
-import { GameCover } from '@/components/game-cover';
 import { LoadingSpinner } from '@/components/loading-spinner';
 import { fetchPacks } from '@/ipc';
-import { cn } from '@/lib/utils';
 import { packsState } from '@/states/App.states';
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { PackSelection } from '@/views/layout/pack-selection';
+import { createFileRoute } from '@tanstack/react-router';
 import { useAtomValue } from 'jotai';
-import { Frown } from 'lucide-react';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -20,50 +18,8 @@ function Index() {
   const packs = useAtomValue(packsState);
 
   return (
-    <div data-name="index-root">
-      {!packs ||
-        (!packs.length && (
-          <div className="flex flex-col items-center">
-            <p className="p-2 text-center text-2xl font-bold uppercase">
-              No Packs Installed!
-            </p>
-            <Frown size={48} />
-          </div>
-        ))}
-      {packs && packs.length ? (
-        <>
-          <p className="p-2 text-center text-2xl font-bold uppercase">
-            Select Pack
-          </p>
-          <div className="flex flex-wrap justify-center gap-4 p-2">
-            {packs.map((pack) => {
-              return (
-                <Link
-                  to="/packs/$packId"
-                  params={{ packId: pack.id }}
-                  key={pack.id}
-                  className={cn(
-                    'flex flex-col items-center',
-                    'relative',
-                    'after:absolute after:inset-0 hover:after:bg-blue-400/50'
-                  )}
-                >
-                  {pack.cover && (
-                    <GameCover
-                      name={pack.cover.name}
-                      image={pack.cover}
-                      className="h-72"
-                    />
-                  )}
-                  <p className="max-w-48 text-center text-xl font-semibold">
-                    {pack.name}
-                  </p>
-                </Link>
-              );
-            })}
-          </div>
-        </>
-      ) : null}
+    <div data-name="route-index">
+      <PackSelection />
     </div>
   );
 }
