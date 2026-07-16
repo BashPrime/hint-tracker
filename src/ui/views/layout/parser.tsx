cf10a858aa0feb96f0990c4ef45776390cb3c278
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-15 21:03:54 -0700

    css

--- src/ui/views/layout/parser.tsx
+++ src/ui/views/layout/parser.tsx
@@ -53,7 +53,7 @@ export function LayoutParser({ elem }: Props) {
       >
         <p
           className={cn(
-            'dark:text-foreground bg-gray-600 text-neutral-50 dark:bg-neutral-900',
+            'dark:text-foreground bg-neutral-700 text-neutral-50 dark:bg-neutral-900',
             'px-2 py-1',
             'text-base font-bold uppercase select-none'
           )}
