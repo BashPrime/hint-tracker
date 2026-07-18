1d549a99da9e04fc4bbf99164f1a36a70ba05a2f
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-17 22:01:52 -0700

    error box

--- src/electron/config.ts
+++ src/electron/config.ts
@@ -1,3 +1,4 @@
+import { dialog } from 'electron';
 import fs from 'fs';
 import { dirname } from 'path';
 import {
@@ -41,17 +42,33 @@ export function handleCreateUserDataDirs() {
   handleMkDir(USER_TRACKER_SAVES_PATH);
 }
 
-export function saveTrackerState(state: TrackerSaveState, path: string) {
+export function saveTrackerState(
+  state: TrackerSaveState,
+  path: string,
+  showErrorBox?: boolean
+) {
   // Make sure directory exists first
   const dir = dirname(path);
   fs.mkdir(dir, { recursive: true }, (err) => {
     if (err) {
-      return console.error(err);
+      if (showErrorBox) {
+        dialog.showErrorBox('Failed to Create Directory', getErrorMsg(err));
+      }
+      return console.error(
+        'saveTrackerState(): Error creating save directory:',
+        getErrorMsg(err)
+      );
     } else {
       // Handle writing file
       const json = JSON.stringify(state, null, 2);
       fs.writeFile(path, json, (err) => {
         if (err) {
+          if (showErrorBox) {
+            dialog.showErrorBox(
+              'Failed to Save Tracker State',
+              `Failed to save ${path}: ${getErrorMsg(err)}`
+            );
+          }
           console.error(
             'saveTrackerState(): Error writing json file:',
             path,
