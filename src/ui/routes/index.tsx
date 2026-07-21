7bf01aeb603ea234572cef5bd29acbdcc5850889
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-21 14:44:29 -0700

    update loading spinners

--- src/ui/routes/index.tsx
+++ src/ui/routes/index.tsx
@@ -5,10 +5,13 @@ import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: Index,
-  pendingComponent: LoadingSpinner,
+  pendingComponent: () => <LoadingSpinner text="Getting Packs..." />,
   loader: async () => {
     await fetchPacks();
   },
+  gcTime: 0,
+  pendingMs: 0,
+  pendingMinMs: 300,
 });
 
 // oxlint-disable-next-line react/only-export-components
