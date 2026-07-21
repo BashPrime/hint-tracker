0f73dd3200de2942e22bb4d47a38322dad8ff420
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-20 22:42:41 -0700

    prettier

--- src/ui/components/ui/input-group.tsx
+++ src/ui/components/ui/input-group.tsx
@@ -124,7 +124,7 @@ function InputGroupInput({
     <Input
       data-slot="input-group-control"
       className={cn(
-        'pl-0 flex-1 rounded-none border-0 bg-transparent shadow-none ring-0 focus-visible:ring-0 aria-invalid:ring-0 dark:bg-transparent',
+        'flex-1 rounded-none border-0 bg-transparent pl-0 shadow-none ring-0 focus-visible:ring-0 aria-invalid:ring-0 dark:bg-transparent',
         className
       )}
       {...props}
@@ -154,6 +154,5 @@ export {
   InputGroupButton,
   InputGroupInput,
   InputGroupText,
-  InputGroupTextarea
+  InputGroupTextarea,
 };
-
