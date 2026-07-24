0b536404116e65807eeea5bff63780b91dd63340
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-24 15:08:40 -0700

    add precompiled scaled icons

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -6,14 +6,21 @@ export function getPreloadPath() {
   return path.join(
     app.getAppPath(),
     isDev() ? '.' : '..',
-    'dist-electron/electron/preload.cjs'
+    'dist-electron',
+    'electron',
+    'preload.cjs'
   );
 }
 
 export function getIconPath() {
+  return path.join(app.getAppPath(), 'assets', 'icon.png');
+}
+
+export function getAboutIconPath() {
   return path.join(
     app.getAppPath(),
     'assets',
-    'icon.png'
-  )
+    'icons',
+    process.platform === 'win32' ? '48x48.png' : '64x64.png'
+  );
 }
