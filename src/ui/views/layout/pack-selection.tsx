4504854d36e01b44289bddef0e0f94f5733800f2
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-24 17:36:33 -0700

    pack selection css

--- src/ui/views/layout/pack-selection.tsx
+++ src/ui/views/layout/pack-selection.tsx
@@ -27,10 +27,13 @@ export function PackSelection() {
     );
   }
 
-  const sortedPacks = packs.sort((a, b) => a.name.localeCompare(b.name))
+  const sortedPacks = packs.sort((a, b) => a.name.localeCompare(b.name));
 
   return (
-    <div className="flex flex-col items-center select-none" data-name="pack-selection">
+    <div
+      className="flex flex-col items-center select-none"
+      data-name="pack-selection"
+    >
       <div
         className="flex w-full flex-col items-center gap-1"
         data-name="selection-header"
@@ -38,8 +41,8 @@ export function PackSelection() {
         <p className="p-2 text-center text-4xl font-bold uppercase">
           Select a Pack
         </p>
-        <div className="h-1.5 w-3/5 min-w-72 bg-[#f4B938]" />
-        <div className="h-1.5 w-3/5 min-w-72 bg-[#ab4d1b]" />
+        <div className="bg-bashprime-yellow h-1.5 w-3/5 min-w-72" />
+        <div className="bg-bashprime-red h-1.5 w-3/5 min-w-72" />
       </div>
       <div className={cn('flex flex-wrap gap-6 p-4')}>
         {sortedPacks.map((pack, idx) => (
@@ -52,13 +55,41 @@ export function PackSelection() {
             <GameCover
               name={pack.cover?.name ?? ''}
               image={pack.cover ?? undefined}
-              className="h-[280px] w-auto group-hover:ring group-hover:brightness-125"
+              className={cn(
+                'h-[280px] w-auto',
+                'group-hover:border-bashprime-red group-hover:border-1 group-hover:brightness-125',
+                'dark:group-hover:border-bashprime-yellow',
+                'shadow-foreground group-hover:shadow-md/25'
+              )}
             />
             <div className="flex items-center gap-2">
-              <p className="text-lg font-semibold">{pack.name}</p>
-              <p className="text-muted-foreground text-sm">v{pack.version}</p>
+              <p
+                className={cn(
+                  'text-lg font-semibold',
+                  'group-hover:text-bashprime-red',
+                  'dark:group-hover:text-bashprime-yellow',
+                  'text-shadow-muted-foreground group-hover:text-shadow-sm/10'
+                )}
+              >
+                {pack.name}
+              </p>
+              <p
+                className={cn(
+                  'text-muted-foreground text-sm',
+                  'group-hover:text-bashprime-red',
+                  'dark:group-hover:text-bashprime-yellow dark:group-hover:brightness-80'
+                )}
+              >
+                v{pack.version}
+              </p>
             </div>
-            <div className="text-muted-foreground flex flex-row gap-1">
+            <div
+              className={cn(
+                'text-muted-foreground flex flex-row gap-1',
+                'group-hover:text-bashprime-red',
+                'dark:group-hover:text-bashprime-yellow dark:group-hover:brightness-80'
+              )}
+            >
               <User size={16} />
               <p className="text-md">{pack.author}</p>
             </div>
