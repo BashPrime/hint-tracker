af910e488dad9c43168820ae648d44e794eac53f
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-26 22:54:12 -0700

    show devtools toggle

--- src/ui/routes/-components/dev-tools.tsx
+++ src/ui/routes/-components/dev-tools.tsx
@@ -1,15 +1,20 @@
+import { isDev } from '@/helpers/utils';
+import { showDevtoolsState } from '@/states/App.states';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { useAtomValue } from 'jotai';
 import { DevTools } from 'jotai-devtools';
 import css from 'jotai-devtools/styles.css?inline';
 
 export function RouterDevTools() {
-  return process.env.NODE_ENV !== 'production' ? (
+  const showDevtools = useAtomValue(showDevtoolsState);
+  return isDev() && showDevtools ? (
     <TanStackRouterDevtools position="bottom-left" />
   ) : null;
 }
 
 export function JotaiDevTools() {
-  return process.env.NODE_ENV !== 'production' ? (
+  const showDevtools = useAtomValue(showDevtoolsState);
+  return isDev() && showDevtools ? (
     <>
       <style>{css}</style>
       <DevTools position="bottom-right" />
