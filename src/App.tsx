10f7e1d81de3fd4749e3a1a8a67a0bf4dd1a41be
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-29 13:32:49 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { QueryClient } from "@tanstack/react-query";
-import { Button } from "./components/ui/button";
+import { QueryClient } from '@tanstack/react-query';
+import { Button } from './components/ui/button';
 
 // create query client
 const queryClient = new QueryClient();
@@ -7,22 +7,22 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div className="flex flex-col gap-4" id="hint-tracker-page">
-      <section id="header" className="flex gap-2 items-center justify-center">
-        <img src="/icon.png" className="w-auto h-20" />
-        <h1 className="text-6xl font-bold text-bashprime-yellow">
+      <section id="header" className="flex items-center justify-center gap-2">
+        <img src="/icon.png" className="h-20 w-auto" />
+        <h1 className="text-bashprime-yellow text-6xl font-bold">
           BashPrime Hint Tracker
         </h1>
       </section>
       <section className="flex justify-center" id="download">
         <a href="#">
-          <Button className="font-bold bg-bashprime-yellow text-2xl p-6">
+          <Button className="bg-bashprime-yellow p-6 text-2xl font-bold">
             Download Now
           </Button>
         </a>
       </section>
       <section>
         <div className="bg-bashprime-yellow h-1.5 w-full" />
-        <div className="bg-bashprime-red h-1.5 w-full mt-1" />
+        <div className="bg-bashprime-red mt-1 h-1.5 w-full" />
       </section>
 
       <section id="packs">
