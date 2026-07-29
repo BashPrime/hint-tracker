92fa2016e12bcf9c7e1656a09a45188d0327ed91
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-29 15:43:09 -0700

    use /hint-tracker/ base path for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col gap-8" id="hint-tracker-page">
       <section id="header" className="flex items-center justify-center gap-2">
-        <img src="/icon.png" className="h-16 w-auto" />
+        <img src="/hint-tracker/icon.png" className="h-16 w-auto" />
         <h1 className="text-bashprime-yellow text-4xl font-bold">
           BashPrime Hint Tracker
         </h1>
