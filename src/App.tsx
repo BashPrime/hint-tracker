3983accfaf34b8ead42f15c1c9398bf22804a84d
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-29 23:40:20 -0700

    undo header change

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ function App() {
     <div className="flex flex-col gap-8" id="hint-tracker-page">
       <section id="header" className="flex items-center justify-center gap-2">
         <img src="/hint-tracker/icon.png" className="h-16 w-auto" />
-        <div className="flex-1 text-center">
-          <h1 className="text-bashprime-yellow text-4xl font-bold">
-            BashPrime Hint Tracker
-          </h1>
-        </div>
+        <h1 className="text-bashprime-yellow text-4xl font-bold">
+          BashPrime Hint Tracker
+        </h1>
       </section>
       <DownloadsSection />
       <Packs />
