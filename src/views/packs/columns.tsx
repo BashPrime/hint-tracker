7e55ec440f699d02256a54305012069c0dce2821
BashPrime <5769306+BashPrime@users.noreply.github.com>
2026-07-29 23:52:27 -0700

    update columns

--- src/views/packs/columns.tsx
+++ src/views/packs/columns.tsx
@@ -1,21 +1,38 @@
+import { Button } from '@/components/ui/button';
 import type { Pack } from '@/lib/types';
 import type { ColumnDef } from '@tanstack/react-table';
 
 export const columns: ColumnDef<Pack>[] = [
   {
     accessorKey: 'name',
-    header: 'Name',
+    header: 'Pack',
+    accessorFn: (row) => row.name,
+    cell: ({ row }) => {
+      const { name, version } = row.original;
+
+      return (
+        <div className="flex flex-col">
+          <p>{name}</p>
+          <p className="text-muted-foreground text-xs">v{version}</p>
+        </div>
+      );
+    },
   },
   {
     accessorKey: 'game',
     header: 'Game',
   },
   {
-    accessorKey: 'version',
-    header: 'Version',
-  },
-    {
     accessorKey: 'author',
-    header: 'Authors',
+    header: 'Author(s)',
+  },
+  {
+    accessorKey: 'downloadUrl',
+    header: undefined,
+    cell: ({ row }) => (
+      <a href={row.getValue('downloadUrl')} target="_blank">
+        <Button className="bg-bashprime-yellow">Download</Button>
+      </a>
+    ),
   },
 ];
